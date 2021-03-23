6588ad99a32864694b682982eefed31e80a0e7ae
Francisco Tarantuviez <tarantuviezf@gmail.com>
2021-03-23 17:58:17 -0300

    fe

--- main.py
+++ main.py
@@ -2,82 +2,79 @@ import pandas as pd
 import streamlit as st
 import altair as alt
 
-def main():
-  st.set_page_config(page_title='Bioinformatics DNA Count', page_icon="./f.png")
 
-  st.write(""" 
-    # DNA Nucleotide Count Web App
+st.set_page_config(page_title='Bioinformatics DNA Count', page_icon="./f.png")
 
-    This app counts the nucleotide composition of query DNA!
-  """)
+st.write(""" 
+# DNA Nucleotide Count Web App
 
-  st.header('Enter DNA Sequence')
+This app counts the nucleotide composition of query DNA!
+""")
 
-  sequence_input = ">DNA Query 2\nGAACACGTGGAGGCAAACAGGAAGGTGAAGAAGAACTTATCCTATCAGGACGGAAGGTCCTGTGCTCGGG\nATCTTCCAGACGTCGCGACTCTAAATTGCCCCCTCTGAGGTCAAGGAACACAAGATGGTTTTGGAAATGC\nTGAACCCGATACATTATAACATCACCAGCATCGTGCCTGAAGCCATGCCTGCTGCCACCATGCCAGTCCT"
+st.header('Enter DNA Sequence')
 
-  sequence = st.text_area("Sequence Input", sequence_input, height=250)
-  sequence = sequence.splitlines()
-  sequence = sequence[1:]
-  sequence = ''.join(sequence)
+sequence_input = ">DNA Query 2\nGAACACGTGGAGGCAAACAGGAAGGTGAAGAAGAACTTATCCTATCAGGACGGAAGGTCCTGTGCTCGGG\nATCTTCCAGACGTCGCGACTCTAAATTGCCCCCTCTGAGGTCAAGGAACACAAGATGGTTTTGGAAATGC\nTGAACCCGATACATTATAACATCACCAGCATCGTGCCTGAAGCCATGCCTGCTGCCACCATGCCAGTCCT"
 
-  st.write("""
-  ***
-  """)
+sequence = st.text_area("Sequence Input", sequence_input, height=250)
+sequence = sequence.splitlines()
+sequence = sequence[1:]
+sequence = ''.join(sequence)
 
-  st.header('INPUT (DNA Query)')
+st.write("""
+***
+""")
 
-  st.header('OUTPUT (DNA Nucleotide Count)')
+st.header('INPUT (DNA Query)')
 
-  # 1. Print count of each nucleotide
-  st.subheader('1. Print dictionary')
+st.header('OUTPUT (DNA Nucleotide Count)')
 
-  def DNA_nucleotide_count(seq):
-    d = dict([
-      ('A', seq.count('A')),
-      ('T', seq.count('T')),
-      ('G', seq.count('G')),
-      ('C', seq.count('C')),
-    ])
-    return d
+# 1. Print count of each nucleotide
+st.subheader('1. Print dictionary')
 
-  X = DNA_nucleotide_count(sequence)
-  X_label = list(X)
-  X_values = list(X.values())
+def DNA_nucleotide_count(seq):
+  d = dict([
+    ('A', seq.count('A')),
+    ('T', seq.count('T')),
+    ('G', seq.count('G')),
+    ('C', seq.count('C')),
+  ])
+  return d
 
-  X
+X = DNA_nucleotide_count(sequence)
+X_label = list(X)
+X_values = list(X.values())
 
-  # 2. Print the text
-  st.subheader('2. Print text')
-  st.write('There are ' + str(X['A']) + ' adenine (A)')
-  st.write('There are ' + str(X['T']) + ' thymine (T)')
-  st.write('There are ' + str(X['G']) + ' guanine (G)')
-  st.write('There are ' + str(X['C']) + ' cytosine (C)')
+X
 
-  # 3. Display dataframe
-  st.subheader('3. Display Dataframe')
-  df = pd.DataFrame.from_dict(X, orient="index")
-  df = df.rename({0: "count"}, axis=1)
-  df.reset_index(inplace=True)
-  df = df.rename(columns={"index": "nucleotide"})
-  st.write(df)
+# 2. Print the text
+st.subheader('2. Print text')
+st.write('There are ' + str(X['A']) + ' adenine (A)')
+st.write('There are ' + str(X['T']) + ' thymine (T)')
+st.write('There are ' + str(X['G']) + ' guanine (G)')
+st.write('There are ' + str(X['C']) + ' cytosine (C)')
 
-  # 4. Display Bar Chart using Altair
-  st.subheader('4. Display Bar Chart')
-  p = alt.Chart(df).mark_bar().encode(
-    x='nucleotide',
-    y='count'
-  )
-  p = p.properties(
-    width=alt.Step(80)
-  )
-  st.write(p)
+# 3. Display dataframe
+st.subheader('3. Display Dataframe')
+df = pd.DataFrame.from_dict(X, orient="index")
+df = df.rename({0: "count"}, axis=1)
+df.reset_index(inplace=True)
+df = df.rename(columns={"index": "nucleotide"})
+st.write(df)
 
-  st.write("""
-    ## App repository
+# 4. Display Bar Chart using Altair
+st.subheader('4. Display Bar Chart')
+p = alt.Chart(df).mark_bar().encode(
+  x='nucleotide',
+  y='count'
+)
+p = p.properties(
+  width=alt.Step(80)
+)
+st.write(p)
 
-    [Github](https://github.com/ftarantuviez/Bioinformatics-DNA-Count)
-  """)
+st.write("""
+  ## App repository
 
-if __name__ == "__main__":
-  main()
-  
+  [Github](https://github.com/ftarantuviez/Bioinformatics-DNA-Count)
+""")
+
